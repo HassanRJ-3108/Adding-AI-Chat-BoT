3f5691526861a80c42e4c132801d4592f37fc008
syedabanog-1 <gulzar_bano@yahoo.com>
2025-02-10 10:47:08 +0500

    add commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 import Hero from "@/components/hero";
 import Project from "@/components/project";
 import About from "@/components/about";
-import Skill from "@/components/skills";
 import Footer from "@/components/footer";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-blue-500">
       <Hero />
       <About />
-      <Skill />
+
+      {/* Skills Button */}
+      <div className="flex justify-center my-6">
+        <Link href="/skills">
+          <div className="flex items-center gap-2 bg-pink-500 text-green-500 font-semibold px-4 py-2 rounded-lg shadow-md hover:bg-pink-600 transition">
+            <Image src="/EDUCATION.png" alt="Skills Logo" width={24} height={24} />
+            <span>Skills</span>
+          </div>
+        </Link>
+      </div>
+
       <Project />
       <Footer />
     </div>
