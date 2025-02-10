745c8aed99258c4df93a4de42f610817fa07365c
syedabanog-1 <gulzar_bano@yahoo.com>
2025-02-10 10:56:46 +0500

    add commit

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const About = () => {
   return (
     <div id='about'>
-      <section className="text-gray-400 bg-gray-900 body-font">
+      <section className="text-gray-400 bg-gray-800 body-font">
         <div className="container mx-auto flex px-5 py-24 flex-col md:flex-row items-center">
           {/* Image Section */}
           <div className="w-full md:w-1/2 flex justify-center mb-10 md:mb-0">
@@ -23,7 +23,7 @@ const About = () => {
             <h1 className="text-3xl sm:text-4xl font-extrabold text-blue-600 mb-4">
               About Me
             </h1>
-            <p className="text-lg text-gray-300 leading-relaxed mb-6">
+            <p className="text-lg text-gray-400 leading-relaxed mb-6">
               I am enrolled in the GIAIC where I recently completed the second quarter (Q2), now focusing on AI.
                Through this program, I gained valuable skills in HTML, CSS, TypeScript, Node.js, Next.js, python
                and React. As I move into the third quarter, I am excited to dive deeper into advanced development 
